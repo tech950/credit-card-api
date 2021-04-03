28251901f79d8e36351039d3f60dd5fff5d1648d
tech950 <rosemolthomas@gmail.com>
2021-04-03 21:25:47 +0100

    first commit

---
+++ src/main/java/com/zzz/creditcardapi/CreditCardApiApplication.java
@@ -0,0 +1,13 @@
+package com.zzz.creditcardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zzz/creditcardapi/CreditCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzz.creditcardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
