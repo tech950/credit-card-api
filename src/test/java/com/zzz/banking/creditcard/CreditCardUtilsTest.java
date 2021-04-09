08a42cd06e44a1ddccbdd5237d225ec5908a4081
tech950 <rosemolthomas@gmail.com>
2021-04-09 14:48:52 +0100

    ParameterizedTest

--- src/test/java/com/zzz/banking/creditcard/CreditCardUtilsTest.java
+++ src/test/java/com/zzz/banking/creditcard/CreditCardUtilsTest.java
@@ -2,32 +2,20 @@ package com.zzz.banking.creditcard;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CreditCardUtilsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"7992739871ABCD", "79927398713", "79927398713000000000", "79927398713891"})
+    public void testInvalidCreditCards(String cardNumber) {
+        Assertions.assertFalse(CreditCardUtils.isValidCreditCard(cardNumber));
+    }
 
     @Test
     public void testValidCreditCard() {
         Assertions.assertTrue(CreditCardUtils.isValidCreditCard("371110201010004"));
-    }
-
-    @Test
-    public void testAlphabeticCreditCard() {
-        Assertions.assertFalse(CreditCardUtils.isValidCreditCard("7992739871ABCD"));
-    }
-
-    @Test
-    public void testMinLengthCreditCard() {
-        Assertions.assertFalse(CreditCardUtils.isValidCreditCard("79927398713"));
-    }
-
-    @Test
-    public void testMaxLengthCreditCard() {
-        Assertions.assertFalse(CreditCardUtils.isValidCreditCard("79927398713000000000"));
-    }
-
-    @Test
-    public void testInvalidLuhnCheckCreditCard() {
-        Assertions.assertFalse(CreditCardUtils.isValidCreditCard("79927398713891"));
     }
 
 }
